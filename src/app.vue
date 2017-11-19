5bc5b81f75db594c2bbb3cbe78bcb5662374c894
cheng87126 <cheng87126@gmail.com>
2017-11-19 22:31:15 +0800

    add input.vue

--- src/app.vue
+++ src/app.vue
@@ -11,24 +11,34 @@
         <v-checkbox name="checkboxs" id="js-react" value="React" v-model="checkboxVal">React</v-checkbox>
         <v-checkbox name="checkboxs" id="js-angular" value="Angular" v-model="checkboxVal">Angular</v-checkbox>
         <div>checkbox value:{{checkboxVal}}</div>
+        <v-input id="test" name="test" placeholder="test input" v-model="testVal" v-on:blur="validInput">text</v-input>
+        <div>input value:{{testVal}}</div>
     </div>
 </template>
 <script>
 import Upload from './upload.vue'
 import Radio from './radio.vue'
 import Checkbox from './checkbox.vue'
+import Input from './input.vue'
 export default {
     name:'app',
     data(){
         return{
             radioVal:'js',
-            checkboxVal:['Vue']
+            checkboxVal:['Vue'],
+            testVal:'testVal'
         }
     },
     components:{
         'v-upload':Upload,
         'v-radio':Radio,
-        'v-checkbox':Checkbox
+        'v-checkbox':Checkbox,
+        'v-input':Input
+    },
+    methods:{
+        validInput(val){
+            console.log(val)
+        }
     }
 }
 </script>
---
+++ src/input.vue
@@ -0,0 +1,39 @@
+<style>
+
+</style>
+<template>
+    <div>
+        <label v-bind:for="id"><slot></slot></label>
+        <input type="text" 
+            v-bind:id="id"
+            v-bind:name="name"
+            v-bind:value="value"
+            v-bind:placeholder="placeholder"
+            v-on:input="updateVal($event.target.value)"
+            v-on:blur="validData($event.target.value)">
+    </div>
+</template>
+<script>
+export default {
+    name:'input',
+    data(){
+        return{
+
+        }
+    },
+    props:{
+        id:String,
+        name:String,
+        value:String,
+        placeholder:String
+    },
+    methods:{
+        updateVal(val){
+            this.$emit('input',val)
+        },
+        validData(val){
+            this.$emit('blur',val)
+        }
+    }
+}
+</script>
