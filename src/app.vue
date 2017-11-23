0fc84b0c22bb1f6311b8c42402e0db5d343b2c81
cheng87126 <cheng87126@gmail.com>
2017-11-23 23:20:40 +0800

    add button.vue

--- src/app.vue
+++ src/app.vue
@@ -14,6 +14,7 @@
         <v-input id="test" name="test" placeholder="test input" v-model="testVal" v-on:blur="validInput">text</v-input>
         <div>input value:{{testVal}}</div>
         <v-tree v-bind:tree-data="testTree"></v-tree>
+        <v-button size="s" type="success">test</v-button>
     </div>
 </template>
 <script>
@@ -22,6 +23,7 @@ import Radio from './radio.vue'
 import Checkbox from './checkbox.vue'
 import Input from './input.vue'
 import Tree from './tree.vue'
+import Button from './button.vue'
 export default {
     name:'app',
     data(){
@@ -55,7 +57,8 @@ export default {
         'v-radio':Radio,
         'v-checkbox':Checkbox,
         'v-input':Input,
-        'v-tree':Tree
+        'v-tree':Tree,
+        'v-button':Button
     },
     methods:{
         validInput(val){
---
+++ src/button.vue
@@ -0,0 +1,80 @@
+<style>
+.btn{
+    display: inline-block;
+    border-radius:5px; 
+    border: 1px solid skyblue;
+    cursor: pointer;
+    -webkit-transition: all 0.2s;
+    -moz-transition: all 0.2s;
+    transition: all 0.2s;
+}
+.btn:hover{
+    color: #fff;
+    background-color: #00CCCC;
+}
+.btn-s{
+    padding: 5px 10px;
+}
+.btn-m{
+    padding: 10px 20px;
+}
+.btn-l{
+    padding: 15px 30px;
+}
+.success,
+.warning,
+.error{
+    color: #fff;
+}
+.success{
+    background-color: #99CC33;
+}
+.success:hover{
+    background-color: #99CC66;
+}
+.warning{
+    background-color: #FFFF33;
+}
+.warning:hover{
+    background-color: #FFFF66;
+}
+.error{
+    background-color: #FF0033;    
+}
+.error:hover{
+    background-color: #FF3333;
+}
+</style>
+<template>
+    <div class="btn" 
+        v-bind:class="classObject">
+        <slot></slot>
+    </div>
+</template>
+<script>
+export default {
+    name:'button',
+    props:{
+        size:{
+            type:String,
+            default:'m'
+        },
+        type:{
+            type:String,
+            default:'normal'
+        }
+    },
+    computed:{
+        classObject(){
+            return {
+                'btn-s':this.size === 's',
+                'btn-m':this.size === 'm',
+                'btn-l':this.size === 'l',
+                'success':this.type === 'success',
+                'warning':this.type === 'warning',
+                'error':this.type === 'error'
+            }
+        }
+    }
+}
+</script>
