7894a87bc7c4a2fb3fde16d0a9e40a1a5607641c
cheng87126 <cheng87126@gmail.com>
2018-01-17 22:58:48 +0800

    update radio.vue

--- src/app.vue
+++ src/app.vue
@@ -7,7 +7,7 @@
             action="/"
             v-bind:after-upload="testafter"></v-upload>
         <v-radio name="web" id="js-id" value="js" v-bind:checked.sync="radioVal">js</v-radio>
-        <v-radio name="web" id="css-id" value="css" v-bind:checked.sync="radioVal">css</v-radio>
+        <v-radio v-bind:disabled="true" name="web" id="css-id" value="css" v-bind:checked.sync="radioVal">css</v-radio>
         <div>radio value:{{radioVal}}</div>
         <v-checkbox name="checkboxs" id="js-vue" value="Vue" v-model="checkboxVal">Vue</v-checkbox>
         <v-checkbox name="checkboxs" id="js-react" value="React" v-model="checkboxVal">React</v-checkbox>
--- src/radio.vue
+++ src/radio.vue
@@ -2,20 +2,37 @@
 input[type="radio"]{
     display: none;
 }
-input[type="radio"] + label span { 
+input[type="radio"] + label span {
     display: inline-block; 
     vertical-align: middle; 
     width: 20px;
     height: 20px;
     border: 2px solid #888;
-    border-radius: 10px; 
-    background: radial-gradient(#eee, #aaa); 
-}
-input[type="radio"] + label span { 
     border-radius: 50%;
+    margin-right: 6px; 
 }
-input[type="radio"]:checked + label span { 
-    background-image: radial-gradient(#9999CC, #9999FF); 
+input[type="radio"] + label span::after{
+    content: '';
+    display: none;
+    width: 10px;
+    height: 10px;
+    border-radius: 50%;
+    position: absolute;
+    margin-left: 5px;
+    margin-top: 5px;
+    background-color: skyblue;
+}
+input[type="radio"]:checked + label span::after{ 
+    display: block;
+}
+input[type="radio"]:disabled + label span {
+    border-color: #bbb;
+}
+input[type="radio"]:disabled + label{
+    color: #bbb;
+}
+input[type="radio"]:disabled + label span::after{
+    background-color: #bbb;
 }
 </style>
 <template>
@@ -24,7 +41,8 @@ input[type="radio"]:checked + label span {
             v-bind:name="name" 
             v-bind:id="id"
             v-bind:value="value"
-            v-model="picked">
+            v-model="picked"
+            v-bind:disabled="disabled">
         <label v-bind:for="id"><span></span><slot></slot></label>
     </div>
 </template>
@@ -40,7 +58,11 @@ export default {
         name:String,
         id:String,
         value:String,
-        checked:String
+        checked:String,
+        disabled:{
+            type:Boolean,
+            default:false
+        }
     },
     computed:{
         picked:{
